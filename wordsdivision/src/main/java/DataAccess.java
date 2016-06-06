557a5a0a1a0b1988ab66a251118cb9ad87aa9a3b
moody6520 <MuDiSJTU@163.com>
2016-06-06 20:59:53 +0800

    finish words division and emotion analysis with docoument and data

---
+++ wordsdivision/src/main/java/DataAccess.java
@@ -0,0 +1,29 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+/**
+ * Created by monkey_d_asce on 16-5-7.
+ */
+public class DataAccess
+{
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+
+    public DataAccess()
+    {
+            mongoClient = new MongoClient( "localhost" ,27017);
+    }
+
+    public MongoCollection<Document> getCollection(String dbName, String collectionName )
+    {
+        MongoDatabase database = mongoClient.getDatabase(dbName);
+        return database.getCollection(collectionName);
+    }
+
+
+
+
+
+}
